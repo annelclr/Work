7b4066d1faaac5554802473d1beb94d30d752712
MartinBoyerpro <57049702+MartinBoyerpro@users.noreply.github.com>
2019-10-27 20:49:24 +0100

    Update Exercice3bis.h

--- Exercice3bis.h
+++ Exercice3bis.h
@@ -7,8 +7,8 @@
 typedef struct Restaurant{
     char nom_restaurant[50];
     char adresse_restaurant[100];
-    float positionx;
-    float positiony;
+    double positionx;
+    double positiony;
     char specialite[50];
 }Restaurant;
 #endif /* Exercice3bis_h */
