37723299834eafc9b5f76e089835c437ff869a36
annelclr <53079370+annelclr@users.noreply.github.com>
2019-10-29 20:23:56 +0100

    Update Exercice3bis.h

--- Exercice3bis.h
+++ Exercice3bis.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+//Déclaration de structure
 typedef struct Restaurant{
     char nom_restaurant[50];
     char adresse_restaurant[100];
@@ -11,4 +13,12 @@ typedef struct Restaurant{
     double positiony;
     char specialite[50];
 }Restaurant;
+
+
+//Déclarations de fonction
+void cherche_restaurant(double x, double y, double rayon_recherche, Restaurant results[]);
+int lire_restaurant(char *chemin, Restaurant restaurants[]);
+void inserer_restaurant(Restaurant restaurant);
+void cherche_par_specialite(double x, double y, char * specialite[], Restaurant results[]);
+
 #endif /* Exercice3bis_h */
