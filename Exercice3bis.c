765eec5941ef94312eaf4f00384098902af2ea98
MartinBoyerpro <57049702+MartinBoyerpro@users.noreply.github.com>
2019-11-01 17:48:25 +0100

    Update Exercice3bis.c

--- Exercice3bis.c
+++ Exercice3bis.c
@@ -251,3 +251,18 @@ void freeMem(char **specialite, char * spec)
     }
     free(spec);
 }
+
+int lire(char *chaine, int longueur)
+{
+    char *positionEntree = NULL;
+    
+    // On lit le texte saisi au clavier
+    fgets(chaine, longueur, stdin);
+    
+        positionEntree = strchr(chaine, '\n'); // On recherche l'"Entrée"
+        if (positionEntree != NULL) // Si on a trouvé le retour à la ligne
+        {
+            *positionEntree = '\0'; // On remplace ce caractère par \0
+        }
+        return 1; // On renvoie 1 si la fonction s'est déroulée sans erreur
+}
